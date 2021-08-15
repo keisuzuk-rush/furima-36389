039333b75a932df711bbbb79ecc1bff1e3e36dc0
keisuzuk-rush <84221784+keisuzuk-rush@users.noreply.github.com>
2021-08-15 13:45:50 +0900

    新規登録、ログイン、ログアウトの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
 					:recoverable, :rememberable, :validatable
+	
+	
+	validates :nickname,         presence: true
+	validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+	VALID_NAME_KANJI_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+	validates :first_name_kanji, presence: true, format: { with: VALID_NAME_KANJI_REGEX }
+	validates :last_name_kanji,  presence: true, format: { with: VALID_NAME_KANJI_REGEX }
+	VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+	validates :first_name_kana,  presence: true, format: { with: VALID_NAME_KANA_REGEX }
+	validates :last_name_kana,   presence: true, format: { with: VALID_NAME_KANA_REGEX }
+	validates :birthday,         presence: true
+
 end
