98fad2e14e311adcf2e0c2e732f4dd0402248aee
keisuzuk-rush <84221784+keisuzuk-rush@users.noreply.github.com>
2021-08-22 12:10:45 +0900

    Formオブジェクトを用いた複数のテーブルへの情報保存

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,29 @@
+class OrdersController < ApplicationController
+
+	def index
+		@item = Item.find(params[:item_id])
+		@order_address = OrderAddress.new
+	end
+
+	def new
+		@order_address = OrderAddress.new
+	end
+
+	def create
+		@item = Item.find(params[:item_id])
+		@order_address = OrderAddress.new(order_params)
+		if @order_address.valid?
+			@order_address.save
+			redirect_to root_path
+		else
+			render :index
+		end
+	end
+
+	private
+
+	def order_params
+		params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number, :order_id, :user_id, :item_id).merge(user_id: current_user.id, item_id: params[:item_id])
+	end
+
+end
---
+++ app/models/order_address.rb
@@ -0,0 +1,21 @@
+class OrderAddress
+	include ActiveModel::Model
+	attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number, :order_id
+
+	with_options presence: true do
+    validates :user_id
+		validates :item_id
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+		validates :prefecture_id, numericality: { other_than: 0 }
+		validates :city
+		validates :house_number
+		validates :telephone_number, format: { with: /\A\d{10,11}\z/ }, numericality: { only_integer: true}
+  end
+	
+
+	def save
+		order = Order.create(user_id: user_id, item_id: item_id)
+		
+		Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, telephone_number: telephone_number, order_id: order.id)
+	end
+end
