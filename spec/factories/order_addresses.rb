a5cf80b2176a270a8ea1bcd2ec85a5138303f0fe
keisuzuk-rush <84221784+keisuzuk-rush@users.noreply.github.com>
2021-08-22 22:09:07 +0900

    Formオブジェクトの単体テストコードの作成

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :order_address do
+    postal_code      { '123-4567' }
+		prefecture_id    { 2 }
+		city             { '横浜市' }
+		house_number     { '青山１−１' }
+		telephone_number { '0123456789' }
+  end
+end
