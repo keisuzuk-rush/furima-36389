51f55058d6ea4692ec80cab48c90b0ea8fb8bfd5
keisuzuk-rush <84221784+keisuzuk-rush@users.noreply.github.com>
2021-08-23 18:20:50 +0900

    商品購入ページの遷移先の調整

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,8 @@
 class OrdersController < ApplicationController
+	before_action :move_to_index, only: :index
 	before_action :set_item, only: [:index, :create]
 	before_action :set_order_address, only: [:index, :new]
+	before_action :user_check, only: :index
 
 	def index
 	end
@@ -21,10 +23,14 @@ class OrdersController < ApplicationController
 
 	private
 
+	def move_to_index
+    redirect_to root_path unless user_signed_in?
+  end
+
 	def order_params
 		params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number, :order_id, :user_id, :item_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
 	end
-
+	
 	def pay_item
 		Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
 		Payjp::Charge.create(
@@ -33,13 +39,17 @@ class OrdersController < ApplicationController
 			currency: 'jpy'
 		)
 	end
-
+	
 	def set_item
 		@item = Item.find(params[:item_id])
 	end
-
+	
 	def set_order_address
 		@order_address = OrderAddress.new
+	end
+
+	def user_check
+		redirect_to root_path if	@item.user == current_user
 	end
 
 end
